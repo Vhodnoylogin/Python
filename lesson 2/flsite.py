2dda4f436bb26102fd3e42bfa2f487ecfea59084
Радимир Кармацкий <kill.spama@gmail.com>
2020-06-15 19:12:06 +0300

    lesson 2 create html's

--- lesson 2/flsite.py
+++ lesson 2/flsite.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+
 
 @app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
-	return render_template('about.html')
+	return render_template('about.html', title="О сайте", menu=menu)
 
 
 if __name__ == "__main__":
