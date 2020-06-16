aaf0fef110c99b4250a10c8d26758af629f3fecf
Elliott <kill.spama@gmail.com>
2020-06-16 16:32:42 +0300

    test request context

--- lesson 4/flsite.py
+++ lesson 4/flsite.py
@@ -8,15 +8,18 @@ menu = ["Установка", "Первое приложение", "Обратн
 @app.route("/")
 @app.route("/index")
 def index():
-	print(url_for('index'))
-	return render_template('index.html', menu=menu)
+    print(url_for('index'))
+    return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
-	print(url_for('about'))
-	return render_template('about.html', title="О сайте", menu=menu)
+    print(url_for('about'))
+    return render_template('about.html', title="О сайте", menu=menu)
 
 
-if __name__ == "__main__":
-	app.run(debug=True)
+with app.test_request_context():
+    print(url_for('index'))
+
+# if __name__ == "__main__":
+#	app.run(debug=True)
