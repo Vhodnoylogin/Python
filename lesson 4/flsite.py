666dc5eff8c4029e105c30eab9603ff96a81959d
Elliott <kill.spama@gmail.com>
2020-06-16 16:37:22 +0300

    set username route

--- lesson 4/flsite.py
+++ lesson 4/flsite.py
@@ -2,24 +2,29 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
     print(url_for('about'))
-    return render_template('about.html', title="О сайте", menu=menu)
+    return render_template('about.html', title='О сайте', menu=menu)
 
 
-with app.test_request_context():
-    print(url_for('index'))
+@app.route('/profile/<username>')
+def profile(username):
+    return f'Пользователь: {username}'
 
-# if __name__ == "__main__":
-#	app.run(debug=True)
+
+# with app.test_request_context():
+#     print(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
