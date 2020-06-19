ec12f6d169d56fe158f6cdeae64f2736776fc50c
Радимир Кармацкий <kill.spama@gmail.com>
2020-06-19 15:54:55 +0300

    use flash msgs

--- lesson 6/flsite.py
+++ lesson 6/flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
 
@@ -30,7 +30,11 @@ def profile(username):
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     if request.method == 'POST':
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено')
+        else:
+            flash('Ошибка отправки')
+
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
