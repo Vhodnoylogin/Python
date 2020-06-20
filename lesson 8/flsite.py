bdf483a6b747a11f38305358a266689d17c61b32
Радимир Кармацкий <kill.spama@gmail.com>
2020-06-20 23:56:38 +0300

    lesson 8 config app

--- lesson 8/flsite.py
+++ lesson 8/flsite.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import os
+
+from flask import Flask
+
+# config
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'QQL'
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'QQL'
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
 
 menu = [
     {"name": 'Установка', "url": 'install-flask'}
@@ -11,56 +21,50 @@ menu = [
 ]
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    print(url_for('index'))
-    return render_template('index.html', menu=menu)
-
-
-@app.route('/about')
-def about():
-    print(url_for('about'))
-    return render_template('about.html', title='О сайте', menu=menu)
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-    return f'Пользователь: {username}'
-
-
-@app.route('/contact', methods=['POST', 'GET'])
-def contact():
-    if request.method == 'POST':
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='error')
-
-    return render_template('contact.html', title='Обратная связь', menu=menu)
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == 'qql' and request.form['psw'] == '123':
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-    return render_template('login.html', title='Авторизация', menu=menu)
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page404.html', title='Страница не была найдена', menu=menu), 404
-
-
-# with app.test_request_context():
+# @app.route('/')
+# @app.route('/index')
+# def index():
 #     print(url_for('index'))
+#     return render_template('index.html', menu=menu)
+#
+#
+# @app.route('/about')
+# def about():
 #     print(url_for('about'))
-#     print(url_for('profile', username='qql'))
+#     return render_template('about.html', title='О сайте', menu=menu)
+#
+#
+# @app.route('/profile/<username>')
+# def profile(username):
+#     if 'userLogged' not in session or session['userLogged'] != username:
+#         abort(401)
+#     return f'Пользователь: {username}'
+#
+#
+# @app.route('/contact', methods=['POST', 'GET'])
+# def contact():
+#     if request.method == 'POST':
+#         if len(request.form['username']) > 2:
+#             flash('Сообщение отправлено', category='success')
+#         else:
+#             flash('Ошибка отправки', category='error')
+#
+#     return render_template('contact.html', title='Обратная связь', menu=menu)
+#
+#
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     if 'userLogged' in session:
+#         return redirect(url_for('profile', username=session['userLogged']))
+#     elif request.method == 'POST' and request.form['username'] == 'qql' and request.form['psw'] == '123':
+#         session['userLogged'] = request.form['username']
+#         return redirect(url_for('profile', username=session['userLogged']))
+#     return render_template('login.html', title='Авторизация', menu=menu)
+#
+#
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('page404.html', title='Страница не была найдена', menu=menu), 404
 
 
 if __name__ == "__main__":
